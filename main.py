ecab723609ecb1f4036ff91566ac532cd8cad9d3
Snoekoog <b.van.berkel.nl@gmail.com>
2023-10-09 16:02:29 +0200

    Changed link in example script

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import time
 if __name__ == "__main__":
 
     # Link to desired stream (preferably .m3u8 format)
-    my_link = "https://hd-auth.skylinewebcams.com/live.m3u8?a=7tlsqah62kp57gvmvdkljcmub3"
+    # my_link = "https://hd-auth.skylinewebcams.com/live.m3u8?a=7tlsqah62kp57gvmvdkljcmub3" # seems to have some weird anti-scrape protection?
+    my_link = "https://stream.sob.m-dn.net/live/sb1/vKVhWPO2ysiYNGrNfA+Krw1/stream.m3u8?plain=true"
 
     # Create StreamCapture object
     my_cap = StreamCapture(my_link)
